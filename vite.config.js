bf83f4d74acf98fa0845c1a20c0fb47c4f6c73bd
Arxhaven <archit3663@gmail.com>
2026-06-03 13:50:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // (or vue, vanilla, etc.)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/SwarmDB-Demo/',  // 👈 ADD THIS EXACT LINE WITH YOUR REPO NAME
 })
